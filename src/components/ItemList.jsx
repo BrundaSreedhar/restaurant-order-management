9ca7c4c4e98fedd70b75c981f9c6292f402bbd2b
Brunda Sreedhar <brundasreedhar@gmail.com>
2025-11-15 00:38:20 +0000

    added new item

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -9,6 +9,7 @@ const menuItems = [
   { id: 6, name: 'Fish & Chips', price: 15.99, description: 'Battered fish with fries' },
   { id: 7, name: 'Chocolate Cake', price: 6.99, description: 'Rich chocolate dessert' },
   { id: 8, name: 'Ice Cream', price: 4.99, description: 'Vanilla ice cream' },
+  { id: 9, name: 'Ice Cream', price: 4.99, description: 'Vanilla ice cream' },
 ]
 
 function ItemList({ onAddToCart }) {
